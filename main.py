54f0e55175a4c789a0faff682aa5b0923882162a
SigSigSigurd <tatsu.eliason@gmail.com>
2020-07-17 12:11:25 -0500

    Basic console input

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
+import re
+import string
+
 from rymscraper.rateyourmusic import RateYourMusic
 
+#RYM object
 rym = RateYourMusic()
+
+def sub(arg: string, comm: string):
+    return arg[len(comm):len(arg)]
+
+def artist(arg: string):
+    a = sub(arg,"artist ")
+    print(rym.sArtistTop(a, 0))
+
+def null():
+    pass
+
+#handles inputs
+def comm(arg: string):
+    d = {
+        "artist": artist
+    }
+    y = None
+    for i in d.keys():
+        r = r'^'+re.escape(i)
+        x = re.search(r, arg)
+        if x!=None:
+            c = d.get(x.group())
+            return c
+
 class Main:
 
-    print(rym.sArtist("Radiohead"))
+    def main():
+        while True:
+            i = input(">")
+            c = comm(i)
+            if c!=None:
+                c(i)
+
+    if __name__ == "__main__":
+        main()
--- rymscraper/rateyourmusic.py
+++ rymscraper/rateyourmusic.py
@@ -8,11 +8,16 @@ class RateYourMusic:
         self.s = HTMLSession()
         pass
 
-    def sArtist(self, s: string):
+    def sArtistTop(self, s: string, n: int):
         r = self.s.get(sURL()+"&searchtype=a&searchterm="+s).html
-        res =list(r.find('.infobox', first=False))
-        return res[0].text
-
+        res = r.find('.infobox', first=False)
+        for l in res[n].absolute_links:
+            if "https://rateyourmusic.com/artist/" in l:
+                aURL = l
+                break
+        r = self.s.get(aURL).html
+        res = r.find('.artist_info', first=True)
+        return res.text
 
 def sURL():
     return "https://rateyourmusic.com/search?bx="+"".join(random.choice(string.ascii_lowercase+string.digits) for i in range(32))
