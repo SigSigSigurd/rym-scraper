5e86a46345ce1bfdcb28f38894353b368043cfe6
SigSigSigurd <tatsu.eliason@gmail.com>
2020-07-17 13:06:27 -0500

    Streamline script

--- main.py
+++ main.py
@@ -12,23 +12,22 @@ def sub(arg: string, comm: string):
 def artist(arg: string):
     a = sub(arg,"artist ")
     n = re.search(r'\-\d*', a)
+    a = re.sub(r'\-\d', '', a)
     if re.sub(r'\s', '', a)=="":
         print("Filler!")
+    elif re.match(r'\-l', a):
+        a = re.sub(r'\-l', '', a)
+        b = rym.artists(a)
+        if len(b)>1:
+            i = 0
+            for y in b:
+                i+=1
+                z = y.find('b', first=True)
+                print(str(i)+". "+z.text)
+            print ("Specify with -# flag! (ie. artist -"+str(i)+a+" to get \""+z.text+"\")")
     else:
-        if n==None:
-            x = 0
-            b = rym.artists(a)
-            if len(b)>1:
-                print("Did you mean:")
-                i = 0
-                for y in b:
-                    i+=1
-                    z = y.find('b', first=True)
-                    print(str(i)+". "+z.text)
-                print ("Specify with -# flag!")
-        else:
-            x = int(n.group()[1:len(n.group())])
-            print(rym.artist(a, x))
+        x = 1 if n==None else int(n.group()[1:len(n.group())])
+        print(rym.artist(a, x))
 
 def null(arg):
     print("The command: \'"+arg+"\' does not exist!")
